e269b336ce4240a6b04e7bcb7b3df71d5faab002
David Wright <dimwight@ntlworld.com>
2018-08-22 11:13:14 +0100

    Titles

--- src/app/_globals.ts
+++ src/app/_globals.ts
@@ -2,38 +2,6 @@ export * from './Selecting'
 export * from './AppCore'
 export * from './TextContent'
 export * from './ContentingApp'
-export namespace SimpleTitles{
-  export const
-    FirstTextual='First',SecondTextual='Second',
-    Indexing='Choose Item',
-    Index='Index',Indexed='Indexed',IndexStart=0,
-    TextualIndexables=[FirstTextual,SecondTextual],
-    Toggling='Click to toggle',Toggled='Toggled state',
-    Trigger='Click Me!',Triggereds='Button presses',
-    ToggleStart=false,
-    NumericField='Number',NumericLabel='Value',NumericStart=123;
-}
-export namespace SelectingTitles{
-  export const
-    Frame='Frame',
-    Selected='Selected',
-    Chooser='Select Content',
-    Actions='Actions',
-    Live='Live',
-    NewButton='New',
-    ScrollUp='Scroll Up',
-    ScrollDown='Scroll Down',
-    ScrollBy='Scroll By',
-    UpButton='Move Up',
-    BackButton='Back',
-    ForwardButton='Forward',
-    DownButton='Move Down',
-    DeleteButton='Delete',
-    OpenEditButton='Edit',
-    TextEditField='Edit Text',
-    SaveEditButton='Save',
-    CancelEditButton='Cancel',
-    CharsCount='Characters';
-}
+export * from './Titles'
 
 
