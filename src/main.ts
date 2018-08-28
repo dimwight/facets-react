7d5e42ac4f0fdfd1325bb6e27cefc3bc08d1003f
David Wright <dimwight@ntlworld.com>
2018-08-28 13:57:12 +0100

    testAppsLaunch

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import {
   ContentingApp,
   launchApp as testAppsLaunch,
   buildContentingLayout,
-  Specs
+  Specs as TestSpecs
 } from './react/_globals';
 import {launchApp as calendarLaunch} from './calendar/_locals';
-if(true)testAppsLaunch(Specs.AllNonSelecting);
+if(true)testAppsLaunch(TestSpecs.AllNonSelecting);
 else if(true) new ContentingApp(buildContentingLayout).buildSurface();
 else calendarLaunch();
